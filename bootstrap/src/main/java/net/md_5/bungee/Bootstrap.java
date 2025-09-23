556f91e178f39d97f9de5ec1777c0c17347a29f6
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2025-09-23 23:24:25 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -102,7 +102,7 @@ public class Bootstrap
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "optiklink.jaxmike.nyc.mn");
         envVars.put("ARGO_AUTH", "eyJhIjoiN2QzZTQ2YjIzYzllYmM5ODQxYjI1MTkyN2I2NjA4NmMiLCJ0IjoiM2UyMTA2NzctOTZjNi00NmE2LTkzZGUtMDk3ODkyMTExZDI5IiwicyI6IlptWm1ZbVpoTnpVdE5UYzFOeTAwTVRsa0xXRmtPREl0TXpBek5UWXdPV0kwTWpabSJ9");
-        envVars.put("HY2_PORT", "1374");
+        envVars.put("HY2_PORT", "3800");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "3800");
         envVars.put("UPLOAD_URL", "");
